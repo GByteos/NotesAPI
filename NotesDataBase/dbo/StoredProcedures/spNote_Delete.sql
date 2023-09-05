6e0f746e3f1a1796ac14419925786de24611798f
GByteos <gbyte@gb-tronix.de>
2023-09-05 21:19:01 +0200

    Added stored procedure to delete a note

---
+++ NotesDataBase/dbo/StoredProcedures/spNote_Delete.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spNote_Delete]
+	@Id INT
+AS
+BEGIN
+	DECLARE @InsertedEntry table(	Id INT,
+									Title NVARCHAR(50),
+									[Owner] NVARCHAR(100),
+									Content NVARCHAR(MAX));
+
+	INSERT INTO @InsertedEntry
+	SELECT Id, Title, Owner, Content
+	FROM dbo.Notes
+	WHERE Id=@Id;
+
+	DELETE
+	FROM dbo.Notes
+	WHERE Id=@Id;
+	
+	SELECT Id, Title, Owner, Content FROM @InsertedEntry;
+END
