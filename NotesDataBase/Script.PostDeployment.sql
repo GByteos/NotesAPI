23761dc9cf9d3297fe4180c9b8f997fc484d6a2b
GByteos <gbyte@gb-tronix.de>
2023-09-05 20:44:47 +0200

    Added post deployment script to add some sample data in the SQL table. Will only run, if Table is empty.

---
+++ NotesDataBase/Script.PostDeployment.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script						
+--------------------------------------------------------------------------------------
+ Seed some sample Date for development tests				
+--------------------------------------------------------------------------------------
+*/
+if not exists (select 1 from dbo.Notes)
+begin
+	insert into dbo.Notes (Title, Owner, Content)
+	values
+	('This is my 1st note', 'Daniel', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'),
+	('Second note', 'Daniel', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'),
+	('Third note', 'John', 'This is some short text'),
+	('4', 'Diana', 'This is an important note!')
+end
