e0f63b6004066a3856aa200f424e7fa216de725f
GByteos <gbyte@gb-tronix.de>
2023-09-05 21:05:19 +0200

    Added sp for Inserting a note

---
+++ NotesDataBase/dbo/StoredProcedures/spNote_Insert.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spNote_Insert]
+	@Title NVARCHAR(50),
+	@Owner NVARCHAR(100),
+	@Content NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @InsertedEntry table(	Id INT,
+									Title NVARCHAR(50),
+									[Owner] NVARCHAR(100),
+									Content NVARCHAR(MAX));
+
+	INSERT INTO dbo.Notes (Title, [Owner], Content)
+	OUTPUT inserted.Id, inserted.Title, inserted.[Owner], inserted.Content INTO @InsertedEntry
+	VALUES (@Title, @Owner, @Content);
+	
+	SELECT Id, Title, Owner, Content FROM @InsertedEntry;
+END
