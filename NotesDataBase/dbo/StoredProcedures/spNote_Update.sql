8fe714875eb6c96042a37db7533c2e42b8fc9acd
GByteos <gbyte@gb-tronix.de>
2023-09-05 21:10:34 +0200

    Added sp for updating a note

---
+++ NotesDataBase/dbo/StoredProcedures/spNote_Update.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spNote_Update]
+	@Id INT,
+	@Title NVARCHAR(50),
+	@Owner NVARCHAR(100),
+	@Content NVARCHAR(MAX)
+AS
+BEGIN
+	UPDATE dbo.Notes
+	SET Title=@Title, Owner=@Owner, Content=@Content
+	WHERE Id=@Id;
+	
+	SELECT Id, Title, Owner, Content
+	FROM dbo.Notes
+	WHERE Id=@Id;
+END
