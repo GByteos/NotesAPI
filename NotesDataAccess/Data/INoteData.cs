085bf3a2a06d2befdfe041d8913dafc9a19aa24f
GByteos <gbyte@gb-tronix.de>
2023-09-06 16:23:32 +0200

    Moved documentation to interface and added inheridoc

--- NotesDataAccess/Data/INoteData.cs
+++ NotesDataAccess/Data/INoteData.cs
@@ -2,12 +2,43 @@
 
 namespace NotesDataAccess.Data
 {
+    /// <summary>
+    /// Data access and processing for Note models
+    /// </summary>
     public interface INoteData
     {
+        /// <summary>
+        /// Delet a note
+        /// </summary>
+        /// <param name="id">Note id</param>
+        /// <returns>Deleted note</returns>
         Task<NoteModel?> DeleteAsync(int id);
+
+        /// <summary>
+        /// Load all notes
+        /// </summary>
+        /// <returns>List of notes</returns>
         Task<IEnumerable<NoteModel>> GetAllAsync();
+
+        /// <summary>
+        /// Get a note by id
+        /// </summary>
+        /// <param name="id">Id of the note</param>
+        /// <returns>Note or NULL if not found</returns>
         Task<NoteModel?> GetAsync(int id);
+
+        /// <summary>
+        /// Store note
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns>The stored node, including the updated Id</returns>
         Task<NoteModel?> InsertAsync(NoteModel noteModel);
+
+        /// <summary>
+        /// Update a note
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns>The updated note</returns>
         Task<NoteModel?> UpdateAsync(NoteModel noteModel);
     }
 }
--- NotesDataAccess/Data/NoteData.cs
+++ NotesDataAccess/Data/NoteData.cs
@@ -8,9 +8,7 @@ using System.Threading.Tasks;
 
 namespace NotesDataAccess.Data
 {
-    /// <summary>
-    /// Data access and processing for Note models
-    /// </summary>
+    /// <inheritdoc/>
     public class NoteData : INoteData
     {
         private readonly ISqlDataAccess _dataAccess;
@@ -20,31 +18,17 @@ namespace NotesDataAccess.Data
             _dataAccess = dataAccess;
         }
 
-        /// <summary>
-        /// Load all notes
-        /// </summary>
-        /// <returns>List of notes</returns>
         public Task<IEnumerable<NoteModel>> GetAllAsync()
         {
             return _dataAccess.LoadData<NoteModel, dynamic>("dbo.spNote_GetAll", new { });
         }
 
-        /// <summary>
-        /// Get a note by id
-        /// </summary>
-        /// <param name="id">Id of the note</param>
-        /// <returns>Note or NULL if not found</returns>
         public async Task<NoteModel?> GetAsync(int id)
         {
             var result = await _dataAccess.LoadData<NoteModel, dynamic>("dbo.spNote_Get", new { Id = id });
             return result.FirstOrDefault();
         }
 
-        /// <summary>
-        /// Store note
-        /// </summary>
-        /// <param name="noteModel"></param>
-        /// <returns>The stored node, including the updated Id</returns>
         public async Task<NoteModel?> InsertAsync(NoteModel noteModel)
         {
             // TODO: Sanitize input
@@ -54,11 +38,6 @@ namespace NotesDataAccess.Data
             return result.FirstOrDefault();
         }
 
-        /// <summary>
-        /// Update a note
-        /// </summary>
-        /// <param name="noteModel"></param>
-        /// <returns>The updated note</returns>
         public async Task<NoteModel?> UpdateAsync(NoteModel noteModel)
         {
             // TODO: Sanitize input
@@ -68,11 +47,6 @@ namespace NotesDataAccess.Data
             return result.FirstOrDefault();
         }
 
-        /// <summary>
-        /// Delet a note
-        /// </summary>
-        /// <param name="id">Note id</param>
-        /// <returns>Deleted note</returns>
         public async Task<NoteModel?> DeleteAsync(int id)
         {
             var result = await _dataAccess.SaveData<NoteModel, dynamic>(
